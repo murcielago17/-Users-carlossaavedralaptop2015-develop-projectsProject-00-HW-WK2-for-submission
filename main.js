d39b3d8585f822951e411b10c3542153631ffeb7
murcielago17 <murcielago17@hotmail.com>
2015-11-10 12:41:29 -0800

    add logic flow comment

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
+   
+    // bellow here starfunction.
    $('div#blue').click(function() {
+       // below write if statement that brakes animate function 
+       // when width == 200px
+
+       // 
        $(this).animate({
            width: '+=20px'
        });
+
+
    });
    $('div#red').click(function() {
        $(this).animate({
